f5f48e00909080b38c01998f83876275a972a053
Kaio Silveira <silveira.kaio@icloud.com>
2023-03-12 16:53:51 +0000

    replace raw organization record with a class instance

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
-export const organization = { name: 'Acme Gooseberries', country: 'GB' };
+import { Organization } from './organization';
 
+export const organization = new Organization({ name: 'Acme Gooseberries', country: 'GB' });
+
+/**
+ * @deprecated use getOrganization instead
+ */
 export function getRawDataOfOrganization() {
+  return organization._data;
+}
+
+export function getOrganization() {
   return organization;
 }
