b61a76dfeea467ceaa2b2be336534048db19fdaa
Kaio Silveira <silveira.kaio@icloud.com>
2023-03-12 16:55:41 +0000

    introduce getter and setter for name at Organization class

--- src/organization.js
+++ src/organization.js
@@ -2,4 +2,12 @@ export class Organization {
   constructor(data) {
     this._data = data;
   }
+
+  set name(aString) {
+    this._data.name = aString;
+  }
+
+  get name() {
+    return this._data.name;
+  }
 }
