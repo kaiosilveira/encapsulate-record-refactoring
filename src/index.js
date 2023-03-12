61664b0a859d94a7dae04fa5b680366da7631cf2
Kaio Silveira <silveira.kaio@icloud.com>
2023-03-12 17:12:31 +0000

    remove deprecated and unused getRawDataOfOrganization function

--- src/index.js
+++ src/index.js
@@ -2,13 +2,6 @@ import { Organization } from './organization';
 
 export const organization = new Organization({ name: 'Acme Gooseberries', country: 'GB' });
 
-/**
- * @deprecated use getOrganization instead
- */
-export function getRawDataOfOrganization() {
-  return organization._data;
-}
-
 export function getOrganization() {
   return organization;
 }
